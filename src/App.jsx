925f854b938fb9278c6357304a21024646f40709
OĞUZHAN KURTOĞLU <400368@iem.local>
2024-01-06 15:43:19 +0300

    api eklendi axios yuklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import { DatePicker } from "antd";
+import axios from "axios";
 
+const dataFetch = {
+  method: "GET",
+  url: "https://timetable-lookup.p.rapidapi.com/TimeTable/saw/adb/20240110/",
+  headers: {
+    "X-RapidAPI-Key": "142508fb75msh82ad7a1f379ca19p10c5e0jsn935fe1b4e829",
+    "X-RapidAPI-Host": "timetable-lookup.p.rapidapi.com",
+  },
+};
+
+try {
+  const response = await axios.request(dataFetch);
+  console.log(response.data);
+} catch (error) {
+  console.error(error);
+}
 const App = () => {
   return <DatePicker />;
 };
