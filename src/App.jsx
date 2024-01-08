bbdc6a53944bdae632621a3c4b8c4d31a1949b56
OĞUZHAN KURTOĞLU <400368@iem.local>
2024-01-08 16:18:49 +0300

    mock data eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import SearchBar from "./components/SearchBar";
+import data from "./data/data.json"
 
 const App = () => {
   return <div className="h-screen">
   <Navbar />
   <SearchBar/>
+  {console.log(data)}
   </div>
 
 };
--- src/components/SearchBar/Index.jsx
+++ src/components/SearchBar/Index.jsx
@@ -2,30 +2,25 @@ import { DatePicker, Input, Button } from "antd";
 import { GlobalOutlined } from '@ant-design/icons';
 import { useState } from "react";
 
-const SearchBar = () => {
-    // const dataFetch = {
-    //     method: "GET",
-    //     url: "https://timetable-lookup.p.rapidapi.com/TimeTable/saw/adb/20240110/",
-    //     headers: {
-    //       "X-RapidAPI-Key": "142508fb75msh82ad7a1f379ca19p10c5e0jsn935fe1b4e829",
-    //       "X-RapidAPI-Host": "timetable-lookup.p.rapidapi.com",
-    //     },
-    //   }
+  
+  const SearchBar = () =>{ 
     
-
     const [flight, setFlight]= useState({Start :"",End : ""})
     const handleChange = (event) => {
-        setFlight({...flight,[event.target.name]: event.target.value})
-
+    setFlight({...flight,[event.target.name]: event.target.value})}
+    function handleDate(event) {
+      const selectedDate = event.target.value; // Seçilen tarihi alıyoruz
+      console.log("Seçilen tarih:", selectedDate); // Tarihi konsola yazdırıyoruz
     }
+  
 
   return (
     <div className="h-[50%] bg-sky-500 flex flex-col justify-center items-center text-center">
     
-    <div>
+    <div className=" bg-slate-400 h-[300px] w-[400px] p-4 rounded-xl">
 
     <label className="flex flex-col items-center space-x-2">
-      <p className=" font-bold hover:text-xl text-white hover:text-slate-600">Where</p>
+      <p className=" font-bold hover:text-xl text-white hover:text-slate-600">From</p>
       <Input name="Start" className="w-full" onChange={handleChange} />
     </label>
     <label className="flex   flex-col items-center space-x-2">
@@ -35,15 +30,15 @@ const SearchBar = () => {
     <div className="flex w-full justify-evenly items-center">
     <label className="mr-2">
     <p className=" font-bold  hover:text-xl text-white hover:text-slate-600">From</p>
-      <DatePicker className="w-40" />
+      <DatePicker onChange={handleDate}  name="DepartureTime" className="w-40" />
     </label>
     <label>
     <p className=" font-bold  hover:text-xl text-white hover:text-slate-600">To</p>
-      <DatePicker className="w-40" />
+      <DatePicker onChange={handleDate} name="ArrivalTime" className="w-40" />
     </label>
     </div>
    
-    <Button onClick={()=>{console.log(flight)}} className="bg-white mt-4 flex items-center justify-center text-black w-full"><GlobalOutlined /> Explore The World!</Button>
+    <Button onClick={()=>{console.log(flight)}} className="bg-white mt-4 mx-auto flex items-center justify-center text-black"><GlobalOutlined /> Explore The World!</Button>
     </div>
   </div>
   )
